41bedba199587a10773151c73fefd8cbef0ac3f6
musiclvme <wtw2000@126.com>
2021-04-28 16:56:35 +0800

    add debug info for Local minimum value search
    Change-Id: I6cbefe1fe85b515541838679771675be8708d28e

--- 03.LMV_search.c
+++ 03.LMV_search.c
@@ -2,10 +2,15 @@
 
 // find local minimum value in a scope of L length
 
-int array[24] = {100,15,6,7,8,9,10,11,9,10,20,18,17,5,4,3,2,1,3,7,6,5,4};
+int array[24] = {1,2,3,4,
+				 9,5,11,9,
+				 10,20,18,17,
+				 5,4,3,2,
+				 1,3,7,6,
+				 5,4,10,1};
 
 #define BATCH_SIZE 4
-#define L 4
+#define L 2
 
 void main(void)
 {
@@ -17,6 +22,12 @@ void main(void)
 	int loop = len / BATCH_SIZE;
 	int batch_num = 0;
 
+	for (i = 0; i < len; i++) {
+		if ( i % BATCH_SIZE == 0)
+			printf("\n[%02d]:", i);
+		printf("%3d ", array[i]);
+	}
+	printf("\n");
 	for (j = 0; j < loop; j++) {
 		memcpy(&val[0], &array[j*BATCH_SIZE], BATCH_SIZE * sizeof(int));
 		batch_num++;
@@ -31,18 +42,16 @@ void main(void)
 				tmp[i] = val[i];
 			}
 		} else {
-			for (i = 0; i < BATCH_SIZE; i++) {	
+			for (i = 0; i < BATCH_SIZE; i++) {
 				min[i] = min[i] < val[i]? min[i]:val[i];
 				tmp[i] = tmp[i] < val[i]? tmp[i]:val[i];
 			}
-			
 		}
 
+		printf("------end-------batch_num=%d----------------\n", batch_num);
 		printf("min[%d %d %d %d]\n", min[0],min[1],min[2],min[3]);
 		printf("tmp[%d %d %d %d]\n", tmp[0],tmp[1],tmp[2],tmp[3]);
-		printf("-------------batch_num=%d----------------\n", batch_num);
-
+		printf("\n");
 	}
-	
 }
 
